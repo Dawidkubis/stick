1210e64a099fb472ad0415958599384c7e0fd9fc
Dawid J. Kubis <dawidkubis@hitler.rocks>
2020-04-04 19:21:58 +0200

    gitignore

--- stick_proc/target/debug/deps/stick_proc-a22662e9fd4924af.d
+++
@@ -1,5 +0,0 @@
-/home/dawidkubis/git/stick/stick_proc/target/debug/deps/libstick_proc-a22662e9fd4924af.so: src/lib.rs
-
-/home/dawidkubis/git/stick/stick_proc/target/debug/deps/stick_proc-a22662e9fd4924af.d: src/lib.rs
-
-src/lib.rs:
--- stick_proc/target/debug/libstick_proc.d
+++
@@ -1,1 +0,0 @@
-/home/dawidkubis/git/stick/stick_proc/target/debug/libstick_proc.so: /home/dawidkubis/git/stick/stick_proc/src/lib.rs
